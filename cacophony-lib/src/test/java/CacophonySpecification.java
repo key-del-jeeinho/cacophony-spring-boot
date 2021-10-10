d1c96c5c61f29a3bc598e8dbb1c7872460fc27e3
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2021-10-10 21:15:50 +0900

    [ADD] 설정파일 변경 및 스펙 추가

--- cacophony-lib/src/test/java/CacophonySpecification.java
+++ cacophony-lib/src/test/java/CacophonySpecification.java
@@ -1,13 +1,22 @@
+import com.velocia.cacophony.domain.config.CacophonyConfiguration;
 import org.junit.jupiter.api.Test;
+
+import static com.velocia.cacophony.domain.flow.FlowEntry.when;
+import static com.velocia.cacophony.domain.trigger.TriggerEntry.onChat;
 
 public class CacophonySpecification {
     @Test
     public void specification001() {
-        doSomething(
-                event -> System.out.println(event);
-        ).when(
-                onChat()
-                .and().onJoin()
+        when(
+                onChat().and().onJoin()
+        ).doSomething(
+                event -> System.out.println(event.getClass().getSimpleName() + " 가 발생하였습니다!")
         ).complete();
+        when(
+                onChat()
+        ).doSomething(
+                event -> System.out.println(event.getClass().getSimpleName() + "is occured!")
+        ).complete();
+        CacophonyConfiguration.eventRepeater().onGuildMessageReceived(null);
     }
 }

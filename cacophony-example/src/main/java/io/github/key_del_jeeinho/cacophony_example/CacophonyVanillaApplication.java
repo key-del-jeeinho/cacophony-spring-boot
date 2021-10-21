36ea0e8bf439cbd538ff46c82f22d99490381453
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2021-10-21 20:04:39 +0900

    [ADD] Command feature 에 대한 usage spec 작성

--- cacophony-example/src/main/java/io/github/key_del_jeeinho/cacophony_example/CacophonyVanillaApplication.java
+++ cacophony-example/src/main/java/io/github/key_del_jeeinho/cacophony_example/CacophonyVanillaApplication.java
@@ -1,5 +1,6 @@
 package io.github.key_del_jeeinho.cacophony_example;
 
+import io.github.key_del_jeeinho.cacophony_lib.domain.command.component.CommandAction;
 import io.github.key_del_jeeinho.cacophony_lib.domain.event.events.chat.ChatEvent;
 import io.github.key_del_jeeinho.cacophony_lib.domain.event.events.react.ReactEvent;
 import io.github.key_del_jeeinho.cacophony_lib.domain.event.listeners.EventListener;
@@ -7,6 +8,7 @@ import io.github.key_del_jeeinho.cacophony_lib.global.config.CacophonyVanilla;
 
 import static io.github.key_del_jeeinho.cacophony_lib.domain.flow.FlowEntry.when;
 import static io.github.key_del_jeeinho.cacophony_lib.domain.trigger.TriggerEntry.*;
+import static io.github.key_del_jeeinho.cacophony_lib.domain.command.CommandEntry.*;
 
 public class CacophonyVanillaApplication {
     public static void main(String[] args) {
@@ -27,6 +29,23 @@ public class CacophonyVanillaApplication {
                 onReact()
         ).doSomething(
                 (EventListener<ReactEvent>) event -> System.out.println("타입 : " + event.getEventType() + "이모지 : " + event.getEmote())
+        ).complete();
+
+        root("소고야",
+                command("인증",
+                        action((CommandAction) System.out::println),
+                        action((CommandAction) System.out::println)
+                ),
+
+                command("도움말", action(() -> System.out.println("도움말입니다"))),
+
+                command("투표",
+                        command("열기",
+                                action((CommandAction) System.out::println),
+                                action((CommandAction) System.out::println)
+                                ),
+                        action((CommandAction)System.out::println)
+                        )
         ).complete();
     }
 }

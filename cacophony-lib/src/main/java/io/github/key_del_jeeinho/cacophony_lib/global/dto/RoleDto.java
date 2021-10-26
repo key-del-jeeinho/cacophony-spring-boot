2d5cb0b57ffdde616e326d0dcbfec8f5f0ce95db
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2021-10-26 23:31:56 +0900

    [ADD] RoleDto 구현

--- cacophony-lib/src/main/java/io/github/key_del_jeeinho/cacophony_lib/global/dto/RoleDto.java
+++ cacophony-lib/src/main/java/io/github/key_del_jeeinho/cacophony_lib/global/dto/RoleDto.java
@@ -1,5 +1,21 @@
 package io.github.key_del_jeeinho.cacophony_lib.global.dto;
 
-public class RoleDto {
-    //TODO
+import lombok.Getter;
+import net.dv8tion.jda.api.Permission;
+
+import java.awt.*;
+import java.util.List;
+
+@Getter
+public class RoleDto extends DiscordData {
+    public RoleDto(long id, String name, Color color, List<Permission> permissions) {
+        super(id);
+        this.name = name;
+        this.color = color;
+        this.permissions = permissions;
+    }
+
+    private final String name;
+    private final Color color;
+    private final List<Permission> permissions;
 }
